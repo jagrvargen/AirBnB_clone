7f6030c5341e64df555187ef31234f138db94190
Dave Yoon <daveyoon64@gmail.com>
2018-02-11 20:34:09 +0000

    Implement create feature

--- console.py
+++ console.py
@@ -3,11 +3,24 @@
    This module contains the entry point into the AirBnB command interpreter.
 """
 import cmd
-
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """This is the class definition of the command interpreter."""
     prompt = '(hbnb) '
+
+    def do_create(self, arg):
+        """Create a new instance of BaseModel"""
+        if not arg:
+            print("** class name missing **")
+        else:
+            try:
+                a = arg + "()"
+                new = eval(a)
+                new.save()
+                print(new.id)
+            except:
+                print("** class doesn't exist **")
 
     def do_quit(self, arg):
         """Quit out of the command interpreter."""
